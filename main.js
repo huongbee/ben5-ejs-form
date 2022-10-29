3102f1db19c4e3831b058c55d669a3b98d8893f2
HuongNTN <huongntn@payme.vn>
2022-10-29 20:12:45 +0700

    add new port

--- main.js
+++ main.js
@@ -12,4 +12,5 @@ app.use('/', SingerController); //
 app.use('/user', UserController); // /user prefix  user/login , user/sign-up
 app.use('/', ErrorController); //
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+const port = env.port || 3000;
+app.listen(port, () => console.log('Server listening on port 3000'));
